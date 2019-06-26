b30959ee56460283cf4caef80154276e0c1d57be
Maria <lupita.morales1@outlook.com>
2019-06-25 21:06:52 -0400

    update

--- a.py
+++ a.py
@@ -1,9 +1,21 @@
 import random
 
+def check(userinput):
+    if userinput > randomvar:
+        print("Too big")
+    elif userinput < randomvar:
+        print("too small")
+
 randomvar = random.randint(1,100)
 
 print(randomvar)
 
+count = 0
 
-userinput = input("input something")
-print(userinput)
+while True:
+    userinput = input("Input a number 1 to 100 ")
+    count += 1
+    check(userinput)
+    if userinput == randomvar:
+        print('you won ' + str (count))
+        break
